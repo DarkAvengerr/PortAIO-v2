e5328b21d212691058fe49215b36334288c2d893
Berb <tintinridge@gmail.com>
2016-10-21 16:48:44 -0500

    .

---
+++ Champion Ports/Dual-Port/Kennen/Hestia's Kennen/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using LeagueSharp.Common;
+
+namespace Kennen
+{
+    internal class Program
+    {
+
+        public static void Game_OnGameLoad()
+        {
+            try
+            {
+                // ReSharper disable once ObjectCreationAsStatement
+                new Champion.Kennen();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Could not load assembly: {0}", exception);
+                throw;
+            }
+        }
+    }
+}
