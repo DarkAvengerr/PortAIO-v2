09c3ac12ce627eabedd3ba04fa6b26b21f5b04db
Berb <tintinridge@gmail.com>
2016-11-19 10:01:23 -0600

    ElRengarDecentralized updated to latest

--- Core/Champion Ports/Rengar/ElRengarDecentralized/Components/Spells/ISpell.cs
+++ Core/Champion Ports/Rengar/ElRengarDecentralized/Components/Spells/ISpell.cs
@@ -1,5 +1,5 @@
 using EloBuddy; 
- using LeagueSharp.Common; 
+using LeagueSharp.Common; 
  namespace ElRengarDecentralized.Components.Spells
 {
     using System;
--- Core/Champion Ports/Rengar/ElRengarDecentralized/Utils/PrioritizeManager.cs
+++ Core/Champion Ports/Rengar/ElRengarDecentralized/Utils/PrioritizeManager.cs
@@ -1,5 +1,5 @@
 using EloBuddy; 
- using LeagueSharp.Common; 
+using LeagueSharp.Common; 
  namespace ElRengarDecentralized.Utils
 {
     using System;
