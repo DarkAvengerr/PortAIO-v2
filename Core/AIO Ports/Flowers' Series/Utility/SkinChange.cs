77ad5c57a3c8185d5cf62eb2b3088590fa4b77d6
Berb <tintinridge@gmail.com>
2016-11-19 12:34:43 -0600

    Fix ADC_Series loadup

--- Core/AIO Ports/Flowers' Series/Utility/SkinChange.cs
+++ Core/AIO Ports/Flowers' Series/Utility/SkinChange.cs
@@ -15,10 +15,6 @@ using LeagueSharp.Common;
     {
         private static int SkinID;
 
-        private static readonly WebClient Webclient = new WebClient();
-        private static readonly JavaScriptSerializer JSerializer = new JavaScriptSerializer();
-        private static readonly List<SkinInfo> SkinList = new List<SkinInfo>();
-
         private new static readonly Menu Menu = Utilitymenu;
 
         private class SkinInfo
@@ -28,45 +24,10 @@ using LeagueSharp.Common;
 
         static SkinChange()
         {
-            var versionJson = Webclient.DownloadString("http://ddragon.leagueoflegends.com/realms/na.json");
-            var gameVersion =
-                (string)
-                ((Dictionary<string, object>)JSerializer.Deserialize<Dictionary<string, object>>(versionJson)["n"])
-                    ["champion"];
-            var champJson =
-                Webclient.DownloadString(
-                    $"http://ddragon.leagueoflegends.com/cdn/{gameVersion}/data/en_US/champion/{ObjectManager.Player.ChampionName}.json");
-            var skins =
-                (ArrayList)
-                ((Dictionary<string, object>)
-                ((Dictionary<string, object>)
-                    JSerializer.Deserialize<Dictionary<string, object>>(champJson)["data"])[
-                    ObjectManager.Player.ChampionName])["skins"];
-
-            foreach (Dictionary<string, object> skin in skins)
-            {
-                SkinList.Add(new SkinInfo
-                {
-                    Name = skin["name"].ToString().Contains("default")
-                        ? ObjectManager.Player.ChampionName
-                        : skin["name"].ToString()
-                });
-            }
         }
 
         public static void Init()
         {
-            SkinID = Me.SkinId;
-
-            var SkinMenu = Menu.AddSubMenu(new Menu("Skin Change", "Skin Change"));
-            {
-                SkinMenu.AddItem(new MenuItem("EnableSkin", "Enabled", true).SetValue(false)).ValueChanged += EnbaleSkin;
-                SkinMenu.AddItem(
-                    new MenuItem("SelectSkin", "Select Skin: ", true).SetValue(
-                        new StringList(SkinList.Select(x => x.Name).ToArray())));
-            }
-
-            Game.OnUpdate += OnUpdate;
         }
 
         private static void EnbaleSkin(object obj, OnValueChangeEventArgs Args)
