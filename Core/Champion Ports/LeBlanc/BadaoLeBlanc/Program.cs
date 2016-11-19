a3481ade8be73a304ced0b378154ecbc511cfc50
Berb <tintinridge@gmail.com>
2016-11-19 10:14:50 -0600

    BadaoLeBlanc Updated to Latest

--- Core/Champion Ports/LeBlanc/BadaoLeBlanc/Program.cs
+++ Core/Champion Ports/LeBlanc/BadaoLeBlanc/Program.cs
@@ -9,11 +9,17 @@ using SharpDX;
 using Color = System.Drawing.Color;
 
 using EloBuddy; 
- using LeagueSharp.Common; 
+using LeagueSharp.Common; 
  namespace BadaoLeblanc
 {
+    // LeBlanc_Base_P_Tar_Mark.troy Leblanc_Base_P_Tar_Mark_Detonate.troy LeBlanc_Base_P_Timer.troy LeblancPMark
     class Program
     {
+        public static Vector3 LastWPos = new Vector3();
+        public static int LastW =0;
+        public static Obj_AI_Base WaitQTarget = null;
+        public static List<GameObject> PassiveObjects = new List<GameObject>();
+        public static List<GameObject> PassiveCDObjects = new List<GameObject>();
         private static AIHeroClient Player { get { return ObjectManager.Player; } }
 
         public static Orbwalking.Orbwalker Orbwalker;
@@ -25,13 +31,12 @@ using EloBuddy;
         public static int Rstate, Wstate, Ecol;
         public static string QName = "LeblancQ", WName = "LeblancW", EName = "LeblancE", RName = "LeblancRToggle", QRName = "",
             WRName = "", ERName = "", RRName = "LeblancR", W2Name = "LeblancWReturn", RW2Name = "LeblancRWReturn";
-
         public static void Main()
         {
             Game_OnGameLoad();
         }
 
-        public static void Game_OnGameLoad()
+        private static void Game_OnGameLoad()
         {
             if (Player.ChampionName != "Leblanc")
                 return;
@@ -58,6 +63,7 @@ using EloBuddy;
             spellMenu.AddItem(new MenuItem("Use W Back Harass", "Use W Back Harass").SetValue(true));
             spellMenu.AddItem(new MenuItem("Use W Combo", "Use W Combo").SetValue(true));
             spellMenu.AddItem(new MenuItem("Rmode", "R mode").SetValue(new StringList (new string[] { "Q","W","E","Any"},3)));
+            spellMenu.AddItem(new MenuItem("WaitPassive", " Wait for Passive Procs").SetValue(true));
             spellMenu.AddItem(new MenuItem("force focus selected", "force focus selected").SetValue(false));
             spellMenu.AddItem(new MenuItem("if selected in :", "if selected in :").SetValue(new Slider(1000, 1000, 1500)));
             spellMenu.AddItem(new MenuItem("QE Selected Target", "QE Selected Target").SetValue(new KeyBind("G".ToCharArray()[0], KeyBindType.Press)));
@@ -67,6 +73,9 @@ using EloBuddy;
             //Drawing.OnDraw += Drawing_OnDraw;
 
             Game.OnUpdate += Game_OnGameUpdate;
+            GameObject.OnCreate += GameObject_OnCreate;
+            GameObject.OnDelete += GameObject_OnDelete;
+            Spellbook.OnCastSpell += Spellbook_OnCastSpell;
 
 
             Chat.Print("Leblanc by badao updated 10/11/2016!, updated for Lb REWORK!");
@@ -77,16 +86,64 @@ using EloBuddy;
         }
         public static int Rmode { get{ return Menu.Item("Rmode").GetValue<StringList>().SelectedIndex; } }
         public static bool WgapCombo { get { return Menu.Item("Use W Combo Gap").GetValue<bool>(); } }
+        public static bool WaitPassive => Menu.Item("WaitPassive").GetValue<bool>();
+        private static void Spellbook_OnCastSpell(Spellbook sender, SpellbookCastSpellEventArgs args)
+        {
+            if (sender.Owner.IsMe && args.Slot == SpellSlot.W)
+            {
+                LastW = Environment.TickCount;
+                LastWPos = args.StartPosition;
+            }
+            //if (!sender.Owner.IsMe || args.Slot != SpellSlot.Q || !(args.Target as Obj_AI_Base).IsValidTarget())
+            //    return;
+            //var target = args.Target as Obj_AI_Base;
+            //if (GetPassiveState(target) == 0 && !PassiveCDObjects.Any(x => x.Position.Distance(target.Position) <= 20))
+            //{
+            //    WaitQTarget = target;
+            //    LeagueSharp.Common.Utility.DelayAction.Add(500 + Game.Ping, () => WaitQTarget = null);
+            //}
+        }
+        private static void GameObject_OnCreate(GameObject sender, EventArgs args)
+        {
+            if (sender.Name == "LeBlanc_Base_P_Tar_Mark.troy")
+                PassiveObjects.Add(sender);
+            if (sender.Name == "LeBlanc_Base_P_Timer.troy")
+                PassiveCDObjects.Add(sender);
+        }
+        private static void GameObject_OnDelete(GameObject sender, EventArgs args)
+        {
+            if (sender.Name == "LeBlanc_Base_P_Tar_Mark.troy")
+                PassiveObjects.RemoveAll(x => x.NetworkId == sender.NetworkId);
+            if (sender.Name == "LeBlanc_Base_P_Timer.troy")
+                PassiveCDObjects.RemoveAll(x => x.NetworkId == sender.NetworkId);
+        }
         public static void Game_OnGameUpdate(EventArgs args)
         {
             if (Player.IsDead)
                 return;
+            //var target = TargetSelector.GetTarget(1000, TargetSelector.DamageType.Physical);
+            //if (target != null)
+            //{
+            //    var state = GetPassiveState(target);
+            //    if (state ==2)
+            //    {
+            //        W.Cast(Game.CursorPos);
+            //    }
+            //}
             if (Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo)
             {
                 Combo();
             }
             if (Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Mixed)
             {
+                //string buffs = "";
+                //var nearest = HeroManager.Enemies.Where(x => x.IsValidTarget()).MinOrDefault(x => x.Distance(Game.CursorPos));
+                //if (nearest != null)
+                //    foreach (var buff in nearest.Buffs.Where(x => x.Name.ToLower().Contains("leblanc")))
+                //    {
+                //        buffs += buff.Name + (Game.Time - buff.StartTime) + " ;";
+                //    }
+                //Chat.Print(buffs);
                 if (Menu.Item("Use Q Harass").GetValue<bool>())
                 {
                     useQ();
@@ -137,18 +194,28 @@ using EloBuddy;
             if (Selected())
             {
                 var target = TargetSelector.GetSelectedTarget();
-                if (target != null && target.IsValidTarget(Q.Range))
+                if (target != null && target.IsValidTarget(Q.Range) && CanRock(target))
                 {
                     Q.Cast(target);
+                    if (GetPassiveState(target) == 0 && !PassiveCDObjects.Any(x => x.Position.Distance(target.Position) <= 20))
+                    {
+                        WaitQTarget = target;
+                        LeagueSharp.Common.Utility.DelayAction.Add(500 + Game.Ping, () => WaitQTarget = null);
+                    }
                 }
 
             }
             else
             {
                 var target = TargetSelector.GetTarget(Q.Range, TargetSelector.DamageType.Magical);
-                if (target != null && target.IsValidTarget(Q.Range))
+                if (target != null && target.IsValidTarget(Q.Range) && CanRock(target))
                 {
                     Q.Cast(target);
+                    if (GetPassiveState(target) == 0 && !PassiveCDObjects.Any(x => x.Position.Distance(target.Position) <= 20))
+                    {
+                        WaitQTarget = target;
+                        LeagueSharp.Common.Utility.DelayAction.Add(500 + Game.Ping, () => WaitQTarget = null);
+                    }
                 }
             }
         }
@@ -181,7 +248,7 @@ using EloBuddy;
                 if (Selected())
                 {
                     var target = TargetSelector.GetSelectedTarget();
-                    if (target != null && target.IsValidTarget(W.Range))
+                    if (target != null && target.IsValidTarget(W.Range) && CanRock(target))
                     {
                         CastW(target);
                     }
@@ -190,7 +257,7 @@ using EloBuddy;
                 else
                 {
                     var target = TargetSelector.GetTarget(W.Range, TargetSelector.DamageType.Magical);
-                    if (target != null && target.IsValidTarget(W.Range))
+                    if (target != null && target.IsValidTarget(W.Range) && CanRock(target))
                     {
                         CastW(target);
                     }
@@ -203,7 +270,7 @@ using EloBuddy;
             if (Selected())
             {
                 var target = TargetSelector.GetSelectedTarget();
-                if (target != null && target.IsValidTarget(W.Range))
+                if (target != null && target.IsValidTarget(W.Range) && CanRock(target))
                 {
                     CastW(target);
                 }
@@ -212,7 +279,7 @@ using EloBuddy;
             else
             {
                 var target = TargetSelector.GetTarget(W.Range, TargetSelector.DamageType.Magical);
-                if (target != null && target.IsValidTarget(W.Range))
+                if (target != null && target.IsValidTarget(W.Range) && CanRock(target))
                 {
                     CastW(target);
                 }
@@ -235,7 +302,7 @@ using EloBuddy;
             if (Selected())
             {
                 var target = TargetSelector.GetSelectedTarget();
-                if (target != null && target.IsValidTarget(range))
+                if (target != null && target.IsValidTarget(range) && (CanRock(target) || CanRockR()))
                 {
                     CastR(target);
                 }
@@ -244,7 +311,7 @@ using EloBuddy;
             else
             {
                 var target = TargetSelector.GetTarget(range, TargetSelector.DamageType.Magical);
-                if (target != null && target.IsValidTarget(range))
+                if (target != null && target.IsValidTarget(range) && (CanRock(target) || CanRockR()))
                 {
                     CastR(target);
                 }
@@ -361,6 +428,78 @@ using EloBuddy;
                         E.Cast(target);
                     }
                 } 
+        }
+        public static int GetPassiveState(Obj_AI_Base target)
+        {
+            if (!target.HasBuff("LeblancPMark"))
+                return 0;
+            var buff = target.GetBuff("LeblancPMark");
+            if ((Game.Time - buff.StartTime) * 1000 < 1400)
+                return 1;
+            if ((Game.Time - buff.StartTime) * 1000 >= 1400 && (Game.Time - buff.StartTime) * 1000 <= 3800)
+                return 2;
+            return 0;
+        }
+        public static bool CanRock(Obj_AI_Base target)
+        {
+            int state = GetPassiveState(target);
+            if (!WaitPassive)
+                return true;
+            if (state == 2)
+                return true;
+            if (GetDamage(target) >= target.Health)
+                return true;
+            if (R.Instance.Name == RRName && R.Instance.Level >= 1)
+                return true;
+            if (PassiveCDObjects.Any(x => x.Position.Distance(target.Position) <= 20))
+                return true;
+            if (target.HasBuff("LeblancE") && state == 1)
+            {
+                var buffE = target.GetBuff("LeblancE");
+                var buffP = target.GetBuff("LeblancPMark");
+                if (buffP.StartTime * 1000 + 1495 <= buffE.StartTime * 1000 + 1500)
+                    return true;
+            }
+            if (WaitQTarget != null && WaitQTarget.NetworkId == target.NetworkId && state == 0)
+            {
+                return false;
+            }
+            if (LastWPos.IsValid() && LastWPos.Distance(target.Position) <= 350 && Environment.TickCount - LastW <= 1000 + Game.Ping && state == 0)
+                return false;
+            if (state == 1)
+            {
+                return false;
+            }
+            return true;
+        }
+        public static bool CanRockR()
+        {
+            return
+                Q.IsReady()
+                || (W.IsReady() && W.Instance.Name == WName)
+                || E.IsReady();
+        }
+        public static double GetDamage(Obj_AI_Base target)
+        {
+            double damage = 0;
+            if (R.Instance.Name != RRName || R.Instance.Level < 1)
+            {
+                if (Q.IsReady())
+                    damage += Q.GetDamage(target);
+                if (W.IsReady())
+                    damage += W.GetDamage(target);
+                if (Q.IsReady())
+                    damage += E.GetDamage(target) * 2;
+            }
+            if (R.Instance.Name == RName && R.Instance.Level >= 1 && R.IsReady())
+            {
+                damage += Player.CalcDamage(target, Damage.DamageType.Magical, new double[] { 125, 225, 325 }[R.Instance.Level - 1] + 0.5 * Player.TotalMagicalDamage);
+            }
+            if (R.Instance.Name == RRName && R.Instance.Level  >=1)
+            {
+                damage += Player.CalcDamage(target, Damage.DamageType.Magical, new double[] { 125, 225, 325 }[R.Instance.Level - 1] + 0.5 * Player.TotalMagicalDamage);
+            }
+            return damage;
         }
     }
 }
