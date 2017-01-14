69aeb5eaca726a8a657ae55807a7b1d58c466228
Berb <tintinridge@gmail.com>
2017-01-13 18:24:37 -0600

    VayneHunterReborn updated to latest

--- Core/Champion Ports/Vayne/VayneHunterReborn/Skills/Condemn/CondemnLogic.cs
+++ Core/Champion Ports/Vayne/VayneHunterReborn/Skills/Condemn/CondemnLogic.cs
@@ -8,10 +8,9 @@ using VayneHunter_Reborn.External.Evade;
 using VayneHunter_Reborn.Skills.Condemn.Methods;
 using VayneHunter_Reborn.Utility;
 using VayneHunter_Reborn.Utility.MenuUtility;
+using EloBuddy;
 
-using EloBuddy; 
- using LeagueSharp.Common; 
- namespace VayneHunter_Reborn.Skills.Condemn
+namespace VayneHunter_Reborn.Skills.Condemn
 {
     class CondemnLogic
     {
@@ -39,16 +38,17 @@ using EloBuddy;
                 return;
             }
 
-            /**
+
             var CondemnTarget = GetCondemnTarget(ObjectManager.Player.ServerPosition);
             if (CondemnTarget.IsValidTarget())
             {
-               // var AAForE = MenuExtensions.GetItemValue<Slider>("dz191.vhr.misc.condemn.noeaa").Value;
+                var AAForE = MenuExtensions.GetItemValue<Slider>("dz191.vhr.misc.condemn.noeaa").Value;
 
-               // if (CondemnTarget.Health / ObjectManager.Player.GetAutoAttackDamage(CondemnTarget, true) < AAForE)
-               // {
-               //     return;
-               // }
+                if (Math.Ceiling(CondemnTarget.Health / ObjectManager.Player.GetAutoAttackDamage(CondemnTarget, true)) < AAForE)
+                {
+                    return;
+                }
+
                 var targetPosition = CondemnTarget.ServerPosition;
                 var myPosition = ObjectManager.Player.ServerPosition;
                 if (WindWall.CollidesWithWall(myPosition, targetPosition))
@@ -57,75 +57,11 @@ using EloBuddy;
                 }
 
                 E.CastOnUnit(CondemnTarget);
-            }*/
-
-            var pushDistance = MenuExtensions.GetItemValue<Slider>("dz191.vhr.misc.condemn.pushdistance").Value - 25;
-
-             if (pushDistance >= 410)
-            {
-                var PushEx = pushDistance;
-                pushDistance -= (10 + (PushEx - 410)/2);
+                TrinketBush(CondemnTarget.ServerPosition.Extend(ObjectManager.Player.ServerPosition, -450f));
             }
-            foreach (var target in HeroManager.Enemies.Where(en => en.IsValidTarget(E.Range) && !en.IsDashing()))
-            {
-                //Yasuo Windwall check
-                if (WindWall.CollidesWithWall(ObjectManager.Player.ServerPosition, target.ServerPosition))
-                {
-                    continue;
-                }
+            return;
 
-                if (MenuExtensions.GetItemValue<bool>("dz191.vhr.misc.condemn.onlystuncurrent") && Variables.Orbwalker.GetTarget() != null &&
-                            !target.NetworkId.Equals(Variables.Orbwalker.GetTarget().NetworkId))
-                {
-                    continue;
-                }
 
-                if (target.Health + 10 <=
-                    ObjectManager.Player.GetAutoAttackDamage(target) *
-                    MenuExtensions.GetItemValue<Slider>("dz191.vhr.misc.condemn.noeaa").Value)
-                {
-                    continue;
-                }
-                var Prediction = Variables.spells[SpellSlot.E].GetPrediction(target);
-                var endPosition = Prediction.UnitPosition.Extend(ObjectManager.Player.ServerPosition, -pushDistance);
-
-                if (Prediction.Hitchance >= HitChance.VeryHigh)
-                {
-                    if (endPosition.IsWall())
-                    {
-                        var condemnRectangle = new VHRPolygon(VHRPolygon.Rectangle(target.ServerPosition.To2D(), endPosition.To2D(), target.BoundingRadius));
-
-                        if (condemnRectangle.Points
-                            .Count(point => NavMesh.GetCollisionFlags(point.X, point.Y).HasFlag(CollisionFlags.Wall)) >= condemnRectangle.Points.Count() * (20 / 100f))
-                        {
-                            E.CastOnUnit(target);
-                            TrinketBush(target.ServerPosition.Extend(ObjectManager.Player.ServerPosition, -450f));
-                        }
-                    }
-                    else
-                    {
-                        //It's not a wall.
-                        var step = pushDistance / 5f;
-                        for (float i = 0; i < pushDistance; i += step)
-                        {
-                            var endPositionEx = Prediction.UnitPosition.Extend(ObjectManager.Player.ServerPosition, -i);
-                            if (endPositionEx.IsWall())
-                            {
-                                var condemnRectangle = new VHRPolygon(VHRPolygon.Rectangle(target.ServerPosition.To2D(), endPosition.To2D(), target.BoundingRadius));
-                                
-                                if (condemnRectangle.Points
-                                    .Count(point => NavMesh.GetCollisionFlags(point.X, point.Y).HasFlag(CollisionFlags.Wall)) >= condemnRectangle.Points.Count() * (20 / 100f))
-                                {
-                                        E.CastOnUnit(target);
-                                        TrinketBush(target.ServerPosition.Extend(ObjectManager.Player.ServerPosition, -450f));
-
-                                }
-                                return;
-                            }
-                        }
-                    }
-                }
-            }
         }
 
         private static void TrinketBush(Vector3 endPosition)
@@ -146,11 +82,12 @@ using EloBuddy;
         private static void Spellbook_OnCastSpell(Spellbook sender, SpellbookCastSpellEventArgs args)
         {
             return;
-            
+
         }
 
         public static Obj_AI_Base GetCondemnTarget(Vector3 fromPosition)
         {
+            return GetCondemnTarget2();
             switch (MenuExtensions.GetItemValue<StringList>("dz191.vhr.misc.condemn.condemnmethod").SelectedIndex)
             {
                 case 0:
@@ -165,6 +102,89 @@ using EloBuddy;
                 case 3:
                     //Shine#
                     return Shine.GetTarget(fromPosition);
+            }
+            return null;
+        }
+
+        public static AIHeroClient GetCondemnTarget2()
+        {
+            var pushDistance = MenuExtensions.GetItemValue<Slider>("dz191.vhr.misc.condemn.pushdistance").Value - 25;
+
+            if (pushDistance >= 410)
+            {
+                var PushEx = pushDistance;
+                pushDistance -= (10 + (PushEx - 410) / 2);
+            }
+            foreach (var target in HeroManager.Enemies.Where(en => en.IsValidTarget(E.Range) && !en.IsDashing()))
+            {
+                //Yasuo Windwall check
+                if (WindWall.CollidesWithWall(ObjectManager.Player.ServerPosition, target.ServerPosition))
+                {
+                    continue;
+                }
+
+                if (MenuExtensions.GetItemValue<bool>("dz191.vhr.misc.condemn.onlystuncurrent") &&
+                    Variables.Orbwalker.GetTarget() != null &&
+                    !target.NetworkId.Equals(Variables.Orbwalker.GetTarget().NetworkId))
+                {
+                    continue;
+                }
+
+                if (target.Health + 10 <=
+                    ObjectManager.Player.GetAutoAttackDamage(target) *
+                    MenuExtensions.GetItemValue<Slider>("dz191.vhr.misc.condemn.noeaa").Value)
+                {
+                    continue;
+                }
+                var Prediction = Variables.spells[SpellSlot.E].GetPrediction(target);
+                var endPosition = Prediction.UnitPosition.Extend(ObjectManager.Player.ServerPosition, -pushDistance);
+
+                if (Prediction.Hitchance >= HitChance.VeryHigh)
+                {
+                    if (endPosition.IsWall())
+                    {
+                        var condemnRectangle =
+                            new VHRPolygon(
+                                VHRPolygon.Rectangle(
+                                    target.ServerPosition.To2D(), endPosition.To2D(), target.BoundingRadius));
+
+                        if (
+                            condemnRectangle.Points.Count(
+                                point => NavMesh.GetCollisionFlags(point.X, point.Y).HasFlag(CollisionFlags.Wall)) >=
+                            condemnRectangle.Points.Count() * (20 / 100f))
+                        {
+                            E.CastOnUnit(target);
+                            TrinketBush(target.ServerPosition.Extend(ObjectManager.Player.ServerPosition, -450f));
+                        }
+                    }
+                    else
+                    {
+                        //It's not a wall.
+                        var step = pushDistance / 5f;
+                        for (float i = 0; i < pushDistance; i += step)
+                        {
+                            var endPositionEx = Prediction.UnitPosition.Extend(ObjectManager.Player.ServerPosition, -i);
+                            if (endPositionEx.IsWall())
+                            {
+                                var condemnRectangle =
+                                    new VHRPolygon(
+                                        VHRPolygon.Rectangle(
+                                            target.ServerPosition.To2D(), endPosition.To2D(), target.BoundingRadius));
+
+                                if (
+                                    condemnRectangle.Points.Count(
+                                        point =>
+                                            NavMesh.GetCollisionFlags(point.X, point.Y).HasFlag(CollisionFlags.Wall)) >=
+                                    condemnRectangle.Points.Count() * (20 / 100f))
+                                {
+                                    return target;
+
+
+                                }
+                            }
+                        }
+                    }
+                }
             }
             return null;
         }
