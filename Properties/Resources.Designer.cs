8bf496b4cb34a8cb2c85ffa874db3db0e1e7914f
Berb <tintinridge@gmail.com>
2017-01-16 17:54:19 -0600

    Fix Camille Exception

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -412,6 +412,16 @@ namespace PortAIO.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Camille_Square_0 {
+            get {
+                object obj = ResourceManager.GetObject("Camille_Square_0", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Cassiopeia {
             get {
                 object obj = ResourceManager.GetObject("Cassiopeia", resourceCulture);
